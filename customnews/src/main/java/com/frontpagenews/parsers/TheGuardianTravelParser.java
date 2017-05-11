d8026aa7f93ac24a8b936696a36999e3d28280ee
Anda Costina UNGHIANU <aunghianu@bitdefender.com>
2017-05-11 08:04:42 +0300

    [back-end] added mongodump with The Guardian Travel articles

--- customnews/src/main/java/com/frontpagenews/parsers/TheGuardianTravelParser.java
+++ customnews/src/main/java/com/frontpagenews/parsers/TheGuardianTravelParser.java
@@ -9,6 +9,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
 import java.util.Date;
@@ -18,6 +19,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.io.IOException;
 
+@Component
 public class TheGuardianTravelParser {
   @Autowired
   private ArticleService articleService;
