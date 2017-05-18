ecdd8e7a5bb7c08bdc375f62a68ab8c970b02ea5
Raresarv <raresarv1@gmail.com>
2017-05-18 15:09:00 +0300

    Add files via upload

---
+++ HackerNewsParserTest/src/main/java/CrudService.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public interface CrudService<T> {
+    T save(T entity);
+    List<T> getAll();
+    T getById(String id);
+    void delete(String id);
+}
---
+++ HackerNewsParserTest/src/test/java/HackerNewsParserTest.java
@@ -0,0 +1,64 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by raresarv on 5/17/2017.
+ */
+public class HackerNewsParserTest {
+    @Test
+    public void testAdd() throws Exception {
+
+        HackerNewsParser p = new HackerNewsParser();
+
+        List<ArticleModel> articles =  p.parseAll();
+
+        assertTrue(articles.size()>1);
+
+    }
+
+    @Test
+    public void testcontainsDollar() throws Exception{
+
+        HackerNewsParser p = new HackerNewsParser();
+
+        List<ArticleModel> articles =  p.parseAll();
+
+        for(ArticleModel art:articles){
+            boolean containsDollar = art.getTitle().contains("$");
+            assertFalse(containsDollar);
+        }
+    }
+    @Test
+    //checking if the url is Good
+    public void testImageUrl() throws Exception {
+        HackerNewsParser p = new HackerNewsParser();
+
+        List<ArticleModel> articles =  p.parseAll();
+
+        for(ArticleModel art:articles){
+            String imageUrl = art.getImageUrl() ;
+            boolean goodUrl = imageUrl.matches("^(http|https|ftp)://.*$");
+            assertTrue(goodUrl);
+
+        }
+    }
+    @Test
+    public void checkSourceUrl () throws  Exception {
+        HackerNewsParser p = new HackerNewsParser();
+        List<ArticleModel> articles =  p.parseAll();
+
+        for(ArticleModel art:articles){
+            String url = String.valueOf(art.getSource());
+            String content = String.valueOf(art.getContent());
+
+            boolean goodUrl = url.matches("^Site: (http|https)://.*$");
+            assertTrue(!content.isEmpty() && goodUrl );
+        }
+
+    }
+
+}
