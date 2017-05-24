247e590b6acfc394b99ecd513dc7d53f50588613
ioneseigabriel <ioneseigabriel@gmail.com>
2017-05-24 23:28:16 +0300

    Add files via upload
    Summarization

---
+++ customnews/src/main/java/com/frontpagenews/summar/algorithm/KeywordAlgorithm.java
@@ -0,0 +1,17 @@
+package com.frontpagenews.summar.algorithm;
+
+import java.util.List;
+
+/**
+ * Interface for keyword extraction algorithms.
+ */
+public interface KeywordAlgorithm {
+
+    /**
+     * Runs keyword algorithm on tokenized sentence list.
+     *
+     * @param 	sentences	List of lists of strings.
+     * @return	List of keyword strings.
+     */
+    public List<String> getKeywords(List<List<String>> sentences);
+}
