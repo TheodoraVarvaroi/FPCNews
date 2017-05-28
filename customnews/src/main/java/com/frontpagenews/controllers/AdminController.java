062bf5db635f42cd0767a5d9f2aa8ce65781403f
Anda Costina UNGHIANU <aunghianu@bitdefender.com>
2017-05-28 22:27:05 +0300

    back

--- customnews/src/main/java/com/frontpagenews/controllers/AdminController.java
+++ customnews/src/main/java/com/frontpagenews/controllers/AdminController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:8181")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
