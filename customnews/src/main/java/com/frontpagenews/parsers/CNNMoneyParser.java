f8619bc050df17436edff41200562555ed155e5c
ioneseigabriel <ioneseigabriel@gmail.com>
2017-06-01 09:17:34 +0300

    Update CNNMoneyParser.java

--- customnews/src/main/java/com/frontpagenews/parsers/CNNMoneyParser.java
+++ customnews/src/main/java/com/frontpagenews/parsers/CNNMoneyParser.java
@@ -91,9 +91,11 @@ public class CNNMoneyParser {
             article.setTitle(f_title);
             article.setContent(f_content);
 
-            summar=new Summar(f_content);
-            String summary = summar.getSummary();
-            article.setSummary(summary);
+           summar=new Summar(f_content);
+            String shortSummary = summar.getShortSummary();
+            String longSummary=summar.getLongSummary();
+            article.setShortSummary(shortSummary);
+            article.setLongSummary(longSummary);
 
             article.setContentLength(f_content.length());
             article.setImageUrl(f_image);
@@ -131,8 +133,12 @@ public class CNNMoneyParser {
                     f_content2=TranslatorAPI.translate(f_content,language,to1);
                     frenchArticle.setContent(f_content2);
                     frenchArticle.setLanguage(to1.toString());
-                    f_sumar2=TranslatorAPI.translate(summary,language,to1);
-                    frenchArticle.setSummary(f_sumar2);
+                    
+                    f_sumar2=TranslatorAPI.translate(shortSummary,language,to1);
+                    frenchArticle.setShortSummary(f_sumar2);
+                    f_sumar2=TranslatorAPI.translate(longSummary,language,to1);
+                    frenchArticle.setLongSummary(f_sumar2);
+                    
                     frenchArticle.setImageWidth(article.getImageWidth());
                     frenchArticle.setImageHeight(article.getImageHeight());
                     frenchArticle.setSourceTags(article.getSourceTags());
@@ -152,8 +158,12 @@ public class CNNMoneyParser {
                     f_content2=TranslatorAPI.translate(f_content,language,to2);
                     germanArticle.setContent(f_content2);
                     germanArticle.setLanguage(to2.toString());
-                    f_sumar2=TranslatorAPI.translate(summary,language,to2);
-                    germanArticle.setSummary(f_sumar2);
+                    
+                     f_sumar2=TranslatorAPI.translate(shortSummary,language,to1);
+                    germanArticle.setShortSummary(f_sumar2);
+                    f_sumar2=TranslatorAPI.translate(longSummary,language,to1);
+                    germanArticle.setLongSummary(f_sumar2);
+                    
                     germanArticle.setImageWidth(article.getImageWidth());
                     germanArticle.setImageHeight(article.getImageHeight());
                     germanArticle.setSourceTags(article.getSourceTags());
@@ -173,8 +183,12 @@ public class CNNMoneyParser {
                     f_content2=TranslatorAPI.translate(f_content,language,to3);
                     italianArticle.setContent(f_content2);
                     italianArticle.setLanguage(to3.toString());
-                    f_sumar2=TranslatorAPI.translate(summary,language,to3);
-                    italianArticle.setSummary(f_sumar2);
+                    
+                     f_sumar2=TranslatorAPI.translate(shortSummary,language,to1);
+                    italianArticle.setShortSummary(f_sumar2);
+                    f_sumar2=TranslatorAPI.translate(longSummary,language,to1);
+                    italianArticle.setLongSummary(f_sumar2);
+                    
                     italianArticle.setImageWidth(article.getImageWidth());
                     italianArticle.setImageHeight(article.getImageHeight());
                     italianArticle.setSourceTags(article.getSourceTags());
@@ -195,8 +209,12 @@ public class CNNMoneyParser {
                     f_content2=TranslatorAPI.translate(f_content,language,to4);
                     spanishArticle.setContent(f_content2);
                     spanishArticle.setLanguage(to4.toString());
-                    f_sumar2=TranslatorAPI.translate(summary,language,to4);
-                    spanishArticle.setSummary(f_sumar2);
+                    
+                     f_sumar2=TranslatorAPI.translate(shortSummary,language,to1);
+                    spanishArticle.setShortSummary(f_sumar2);
+                    f_sumar2=TranslatorAPI.translate(longSummary,language,to1);
+                    spanishArticle.setLongSummary(f_sumar2);
+                    
                     spanishArticle.setImageWidth(article.getImageWidth());
                     spanishArticle.setImageHeight(article.getImageHeight());
                     spanishArticle.setSourceTags(article.getSourceTags());
